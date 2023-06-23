451bb2cee532b2179711951dc8f2bf447398b7ef
negrovariant <sixto.cea1987@gmail.com>
2023-06-23 10:33:12 -0400

    trabajo portafolio ruby

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def proyect
+  end
+
+  def contact
+  end
+
+  def index
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/proyect'
+  get 'home/contact'
+  get 'home/index'
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "home#index"
+end
